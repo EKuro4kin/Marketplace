4f76990dc3ff53d372d81445421a7e6bf24b79f2
Televizor1945 <kurochkin.egor1993@gmail.com>
2022-10-30 18:02:34 +0300

    Завершил 3 шаг Начал 4 шаг

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -1,12 +1,34 @@
 from django.conf import settings
 from django.db import models
 
+from users.models import User
+
 
 class Ad(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    title = models.CharField(verbose_name="название товара", max_length=50)
+    price = models.PositiveIntegerField()
+    description = models.TextField(verbose_name="описание товара", null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ads')
+    created_at = models.DateTimeField(verbose_name="дата создания объявления", auto_now_add=True)
+    image = models.ImageField(upload_to='django_media/')
+
+    class Meta:
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    text = models.CharField(verbose_name="текст отзыва", max_length=1000)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(verbose_name="дата создания отзыва", auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
+
+    def __str__(self):
+        return self.text
--- skymarket/users/models.py
+++ skymarket/users/models.py
@@ -59,3 +59,10 @@ class User(AbstractBaseUser):
     @property
     def is_user(self):
         return self.role == UserRoles.USER
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return f"{self.first_name}{self.last_name}"
